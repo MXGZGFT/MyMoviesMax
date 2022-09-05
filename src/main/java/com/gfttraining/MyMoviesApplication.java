10c3929fea18b2a6dbf765761da89e0d9385c32b
Max <mxgz@gft.com>
2022-09-05 11:23:29 +0000

    Uploaded Starting Project

---
+++ src/main/java/com/gfttraining/MyMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.gfttraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMoviesApplication.class, args);
+	}
+
+}
